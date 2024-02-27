22f9f4257143a1bd282f839b8679b7b4ee0f4fcf
divy404 <divyanshj360@gmail.com>
2024-02-27 22:34:56 +0530

    TAKING INPUT AND OUTPUT IN C++

---
+++ BASICS/input-output.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char chr;
+    
+    cin >> chr;
+
+    if (chr >= 'A' && chr <= 'Z') {
+        cout << "1" << endl;
+    } else if (chr >= 'a' && chr <= 'z') {
+        cout << "0" << endl;
+    } else {
+        cout << "-1" << endl;
+    }
+
+    return 0;
+}
