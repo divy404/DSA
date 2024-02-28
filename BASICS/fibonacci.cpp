9ea05f175a13d3afd5d24436ddc67853a556bee5
divy404 <divyanshj360@gmail.com>
2024-02-28 22:12:30 +0530

    CALCULATING FIBONACCI

---
+++ BASICS/fibonacci.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int recur(int n)
+{
+    if (n <= 1)
+    {
+        return n;
+    }
+    return recur(n - 1) + recur(n - 2);
+}
+
+int main()
+{
+    int n;
+   cin>>n;
+    
+    cout<<recur(n)<<endl;
+}
