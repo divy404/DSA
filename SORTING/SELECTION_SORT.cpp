317aab6e9a422128fbf2a5ff37b097124419f66b
divy404 <divyanshj360@gmail.com>
2024-03-04 21:05:53 +0530

    SELECTION SORTING

--- SORTING/SELECTION_SORT.cpp
+++ SORTING/SELECTION_SORT.cpp
@@ -1,37 +1,30 @@
-#include<bits/stdc++.h>
 #include<iostream>
-
-
 using namespace std;
-void selection_sort(int arr[], int n) {
-  // selection sort
-  for (int i = 0; i < n - 1; i++) {
-    int mini = i;
-    for (int j = i + 1; j < n; j++) {
-      if (arr[j] < arr[mini]) {
-        mini = j;
+void SELECTION_SORT(int arr[], int n){
+  for(int i=0;i<n-2;i++){
+    int min=i;
+    for(int j=i;j<=n-1;j++){
+      if(arr[j]<arr[min])
+      {
+        min=j;
       }
     }
-    int temp = arr[mini];
-    arr[mini] = arr[i];
-    arr[i] = temp;
+    int temp=arr[min];
+    arr[min]=arr[i];
+    arr[i]=temp;
   }
-
-  cout << "After selection sort: " << "\n";
-  for (int i = 0; i < n; i++) {
-    cout << arr[i] << " ";
-  }
-  cout << "\n";
-
 }
-int main() {
-  int arr[] = {13,46,24,52,20,9};
-  int n = sizeof(arr) / sizeof(arr[0]);
-   cout << "Before selection sort: " << "\n";
-   for (int i = 0; i < n; i++) {
-    cout << arr[i] << " ";
+int main(){
+  int n;
+  cin>>n;
+  int arr[n];
+  for(int i=0;i<n;i++){
+    cin>>arr[i];
+
   }
-  cout << "\n";
-  selection_sort(arr, n);
+  SELECTION_SORT(arr,n);
+  for(int i=0;i<n;i++){
+    cout<<arr[i]<<" ";
+  }
   return 0;
 }
