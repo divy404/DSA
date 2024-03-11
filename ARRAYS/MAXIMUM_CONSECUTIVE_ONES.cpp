5d7559c4a25020a5dc1cfa6329ab74c3f7e469a2
divy404 <divyanshj360@gmail.com>
2024-03-11 22:35:16 +0530

    FINDING MINIMUM CONSECUTIVE

---
+++ ARRAYS/MAXIMUM_CONSECUTIVE_ONES.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int cnt = 0;
+        int maxi = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 1) {
+                cnt++;
+            } else {
+                cnt = 0;
+            }
+            maxi = max(maxi, cnt);
+        }
+        return maxi;
+    }
+};
+
+int main() {
+    int n;
+    cout << "Enter the size of the vector: ";
+    cin >> n;
+
+    vector<int> nums(n);
+    cout << "Enter the elements of the vector (0 or 1): ";
+    for (int i = 0; i < n; ++i) {
+        cin >> nums[i];
+    }
+
+    Solution obj;
+    int ans = obj.findMaxConsecutiveOnes(nums);
+    cout << "The maximum consecutive 1's are " << ans << endl;
+    return 0;
+}
